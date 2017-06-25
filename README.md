de46ee4fe766cea620d355dea610e06f0c684a00
Keisuke Kishida <dodosuke0920@Keis-MBP.local>
2017-06-25 09:33:40 +0900

    Fixed issues at the python code and 3rd sql request.

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # LogsAnalysis
 Udacity/Full Stack Web Development/Project3
 
+<Description>
 logsanalysis.py is a python script, which outputs a text file from a database.
-The database contains three table: authors, articles and log.
-The text file will contain three data, which tells the most popular articles and authors and errors.
+The database, caled newsdata.sql, contains three table: authors, articles and log.
+The text file will include three data, which tells the most popular articles and authors and errors.
 
+<How to implement>
+1. Set up the Vagrant environment
+2. Load the news database using the newsdata.sql file
+3. execute your Python code with python3
 
+<Note>
 The SQL request are as follows:
 #what are the most popular three articles of all time
 select articles.title, count(*) as num
@@ -26,8 +32,9 @@ select authors.name, count(*) as num
   order by num desc;
 
 #On which days did more than 1% of requests lead to errors?
-select to_char(date_trunc('day', time), 'Mon DD, YYYY') as day,
-    round(count(status='404 NOT FOUND' or null)*100.0/count(*), 2), '%') as error
+select to_char(_day, 'FMMonth FMDD, YYYY') as day, error from
+    (select date_trunc('day', time) as _day,
+    round(count(status='404 NOT FOUND' or null)*100.0/count(*), 2) as error
     from log
-    group by day
-    having count(status='404 NOT FOUND' or null)*100.0/count(*) > 1;
+    group by _day
+    having count(status='404 NOT FOUND' or null)*100.0/count(*) > 1) as result;
--- logsanalysis.py
+++ logsanalysis.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 import psycopg2
 
-#SQL Requests
-#what are the most popular three articles of all time
+# SQL Requests
+# what are the most popular three articles of all time
 sql1 = """select articles.title, count(*) as num
     from log, articles
     where log.status='200 OK'
@@ -10,7 +11,7 @@ sql1 = """select articles.title, count(*) as num
     order by num desc
     limit 3;"""
 
-#Who are the most popular article authors of all time?
+# Who are the most popular article authors of all time?
 sql2 = """select authors.name, count(*) as num
     from log, articles, authors
     where log.status='200 OK'
@@ -19,42 +20,48 @@ sql2 = """select authors.name, count(*) as num
     group by authors.name
     order by num desc;"""
 
-#On which days did more than 1% of requests lead to errors?
-sql3 = """select to_char(date_trunc('day', time), 'Mon DD, YYYY') as day,
+# On which days did more than 1% of requests lead to errors?
+sql3 = """select to_char(_day, 'FMMonth FMDD, YYYY') as day, error from
+    (select date_trunc('day', time) as _day,
     round(count(status='404 NOT FOUND' or null)*100.0/count(*), 2) as error
     from log
-    group by day
-    having count(status='404 NOT FOUND' or null)*100.0/count(*) > 1;"""
+    group by _day
+    having count(status='404 NOT FOUND' or null)*100.0/count(*) > 1)
+    as result;"""
 
-#Get data from database with sql requests
+
+# Get data from database with sql requests
 def get_data(sqlrequest):
-  """Get data from database"""
-  db = psycopg2.connect(database="news")
-  c = db.cursor()
-  c.execute(sqlrequest)
-  data = c.fetchall()
-  db.close()
-  return data
+    # Get data from database
+    db = psycopg2.connect(database="news")
+    c = db.cursor()
+    c.execute(sqlrequest)
+    data = c.fetchall()
+    db.close()
+    return data
 
-#Title of the report
+
+# Title of the report
 text = "Report\n\nMost popular articles\n"
 
-#Make string from data with formatting
+# Make string from data with formatting
 data1 = get_data(sql1)
 for i in range(len(data1)):
-    text = text + str(i+1) + ". \"" + data1[i][0] + "\" - " + str(data1[i][1]) + " views\n"
+    text = (text + str(i+1) + ". \"" + data1[i][0] +
+            "\" - " + str(data1[i][1]) + " views\n")
 
 data2 = get_data(sql2)
 text = text + "\nMost popular authors\n"
 for i in range(len(data2)):
-    text = text + str(i+1) + ". " + data2[i][0] + " - " + str(data2[i][1]) + " views\n"
+    text = (text + str(i+1) + ". " + data2[i][0] +
+            " - " + str(data2[i][1]) + " views\n")
 
 data3 = get_data(sql3)
 text = text + "\nErrors (1%+)\n"
 for i in range(len(data3)):
     text = text + " " + data3[i][0] + " - " + str(data3[i][1]) + "%\n"
 
-#Output to a report.txt file
+# Output to a report.txt file
 f = open('report.txt', 'w')
 f.write(text)
 f.close()
